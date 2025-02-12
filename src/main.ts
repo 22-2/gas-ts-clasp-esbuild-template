a4f31da5c9d26f403163c79cec071b549bc07899
22-2 <17890m@protonmail.ch>
2025-02-12 14:45:37 +0900

    シート名を定数化し、存在確認を追加。エラーハンドリングを強化。

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
-import { requestCharCount } from "./count-characters"; // この関数は別途定義が必要です。
+import { DIARY_SHEET, HOURLY_SHEET } from "./constants";
 import { dailyRecord, hourlyRecord } from "./record";
-
 
 //--------------------------------------------------
 // シートごと、日付ごとに記録する関数 (毎日トリガーで実行)
 //--------------------------------------------------
 function executeDaily() {
   const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  const sheet = spreadsheet.getActiveSheet();
-  dailyRecord(sheet);
+  const sheet = spreadsheet.getSheetByName(DIARY_SHEET.NAME);
+  if (sheet) dailyRecord(sheet);
+  else {
+    throw Error("Error: 記録対象のシートが見つかりません"); // 記録対象のシートが見つからなかった場合の処理}
+  }
 }
-
 //--------------------------------------------------
 // 1時間ごとに記録する関数 (毎時トリガーで実行)
 //--------------------------------------------------
 function executeHourly() {
   const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  const sheet = spreadsheet.getActiveSheet();
-  hourlyRecord(sheet); // 記録処理
+  const sheet = spreadsheet.getSheetByName(HOURLY_SHEET.NAME);
+  if (sheet) hourlyRecord(sheet); // 記録処理
+  else {
+    throw Error("Error: 記録対象のシートが見つかりません"); // 記録対象のシートが見つからなかった場合の処理}
+  }
 }
 
-(global as any).executeDaily = executeDaily;
 (global as any).executeHourly = executeHourly;
+(global as any).executeDaily = executeDaily;
